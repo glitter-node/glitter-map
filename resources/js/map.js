6349ad543adbee5272c2af6399e4b350372e78e0
glitter gim <gim@glitter.kr>
2026-03-26 10:52:29 +0900

    fix: leaflet marker asset path via vite import

--- resources/js/map.js
+++ resources/js/map.js
@@ -1,4 +1,16 @@
 import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+
+delete L.Icon.Default.prototype._getIconUrl
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: markerIcon2x,
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow,
+})
 
 const DEFAULT_CENTER = { lat: 37.5665, lng: 126.9780 }
 const DEFAULT_ZOOM = 13
